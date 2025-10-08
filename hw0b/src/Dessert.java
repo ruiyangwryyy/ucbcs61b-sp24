0a76aaa581663e2707128a3191ec47318347d98d
ruiyangwryyy <ruiyangwryyy@outlook.com>
2025-10-08 19:01:01 +0800

    hw0b作业

---
+++ hw0b/src/Dessert.java
@@ -0,0 +1,16 @@
+public class Dessert {
+    public int flavor;
+    public int price;
+    public static int numDesserts = 0;
+    public Dessert(int flavor, int price) {
+        this.flavor = flavor;
+        this.price = price;
+        numDesserts += 1;
+    }
+    public void printDessert() {
+        System.out.println("Flavor: " + this.flavor + ", Price: " + this.price);
+    }
+    public static void main(String[] args) {
+        System.out.println("I love dessert!");
+    }
+}
--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -6,21 +6,36 @@ public class MapExercises {
      * 1, 'b' is 2, 'c' is 3, ..., 'z' is 26.
      */
     public static Map<Character, Integer> letterToNum() {
-        // TODO: Fill in this function.
-        return null;
+        Map<Character, Integer> map = new java.util.TreeMap<>();
+        for (int i=1;i<=26;i++){
+            map.put((char)('a' + i - 1), i);
+        }
+        return map;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
      *  is [1, 3, 6, 7], the returned map goes from 1 to 1, 3 to 9, 6 to 36, and 7 to 49.
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
-        // TODO: Fill in this function.
-        return null;
+        Map<Integer, Integer> map = new java.util.TreeMap<>();
+        for (Integer num : nums) {
+            map.put(num, num * num);
+        }
+        return map;
     }
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
-        // TODO: Fill in this function.
-        return null;
+        Map<String, Integer> map = new java.util.TreeMap<>();
+        for (String word : words) {
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            } else {
+                map.put(word, 1);
+            }
+        }
+        return map;
     }
-}
+       
+    }
+
